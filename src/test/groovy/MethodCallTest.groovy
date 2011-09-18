031f982d7b19aea23bb8a5677f31d45aa7821807
Motoi Washida <wm3@mail.w3ch.jp>
2011-09-18 20:14:02 +0900

    Add tests

--- src/test/groovy/MethodCallTest.groovy
+++ src/test/groovy/MethodCallTest.groovy
@@ -7,6 +7,7 @@ class MethodCallTest extends Specification {
 	interface DoSomething {
 
 		void 何かする(o)
+		void 何かする(o1, o2)
 
 		interface Overloaded {
 			void 何かする()
@@ -35,5 +36,27 @@ class MethodCallTest extends Specification {
 
 		then: 1 * subject.何かする()
 		and:  1 * subject.何かする(o)
+	}
+
+	def "pass expressions as params"() {
+		def subject = Mock(DoSomething)
+		def 対象 = Wa.japanese(subject)
+
+		when: 対象.が (1 + 2) を何かする()
+		and:  対象.が (1 + 2) で (3 + 4) を何かする()
+
+		then: 1 * subject.何かする(3)
+		and:  1 * subject.何かする(3, 7)
+	}
+
+	def "pass expressions as params without parentheses"() {
+		def subject = Mock(DoSomething)
+		def 対象 = Wa.japanese(subject)
+
+		when: 対象.が (1 + 2) で 4 を何かする()
+		and:  対象.が 1 で (3 + 4) を何かする()
+
+		then: 1 * subject.何かする(3, 4)
+		and:  1 * subject.何かする(1, 7)
 	}
 }
